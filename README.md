38ae28122a8aa8df4c79b5e51f2f0606add681a1
lukme <lukme00@gmail.com>
2019-10-01 15:58:49 +0200

    add option to create and remove tasks

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# deadline
+# DeadLine
+> DeadLine is a work menagement aplication made to help you prioritize your tasks
+
+--- Project in development
+
+## About project
+Deadline was created due to small amount of apps that help you visualize how much time do you still have to acomplish your goals, finish your work projects, or even do your homework. Although DeadLine is based on localStorage, therefore you can not save your tasks on the server, you can still use it and it will store all your tasks in the device you are working on.
--- script/index.js
+++ script/index.js
@@ -1,16 +1,66 @@
-let btn = document.querySelector('.addHandle'),
+let ul = document.querySelector('.tasks'),
+    addBtn = document.querySelector('.addHandle'),
+    addSubmitBtn = document.querySelector('.addSubmit'),
     addPanel = document.querySelector('.addPanel'),
     addContent = document.querySelector('.addContent'),
     count = false;
-btn.addEventListener('click', function() {
+
+// Open and close add panel
+addBtn.addEventListener('click', menageAddPanel);
+
+function menageAddPanel() {
     if (!count) {
         count = !count;
         addPanel.className = "addPanel addOpened";
         addContent.className = "addContent shown";
-
     } else {
         count = !count;
         addPanel.className = "addPanel addClosed";
         addContent.className = "addContent hidden";
     }
-})
+}
+
+function createElement() {
+    const taskData = [
+        document.querySelector('.addContent').children[1],
+        document.querySelector('.addContent').children[2],
+        document.querySelector('.addContent').children[3]
+    ];
+    if (taskData[0].value && taskData[1].value && taskData[2].value) {
+        const li = document.createElement('LI'),
+            div = document.createElement('DIV'),
+            percentage = document.createElement('P'),
+            deadline = document.createElement('P'),
+            name = document.createElement('P'),
+            finishedBtn = document.createElement('BUTTON'),
+            endDate = `${taskData[2].value.slice(8, 10)}.${taskData[2].value.slice(5, 7)}.${taskData[2].value.slice(0, 4)}`;
+        li.classList.add('task');
+        div.classList.add('progressBar', 'firstPeriod');
+        percentage.classList.add('taskPercentage');
+        deadline.classList.add('taskDeadline');
+        name.classList.add('taskName');
+        finishedBtn.classList.add('taskFinished');
+        finishedBtn.addEventListener('click', removeElement);
+        deadline.innerText = endDate;
+        name.innerText = taskData[0].value;
+        li.appendChild(div);
+        li.appendChild(percentage);
+        li.appendChild(deadline);
+        li.appendChild(name);
+        li.appendChild(finishedBtn);
+        ul.insertBefore(li, ul.children[0]);
+        taskData.forEach(el => {
+            el.value = "";
+        });
+        menageAddPanel();
+    } else { return };
+}
+
+function removeElement() {
+    this.parentNode.parentNode.removeChild(this.parentNode);
+}
+
+addSubmitBtn.addEventListener('click', createElement);
+
+
+
